c6cd5524f30199c0e30bcc9b09e0dc89902f783c
Maxime LALO <maxime.lalo.pro@gmail.com>
2021-04-25 21:50:09 +0200

    :sparkles: Creating veterinary middleware

---
+++ middlewares/veterinary.middleware.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import {AuthController} from "../controllers/auth.controller";
+
+export async function veterinaryMiddleware(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const auth = req.headers["authorization"];
+    if (auth !== undefined){
+        const token = auth.slice(7);
+        const authController = await AuthController.getInstance();
+        const role = await authController.getRole(token);
+        if (role !== null) {
+            if (role.name_role === "Veterinary"){
+                next();
+                return;
+            }else{
+                res.status(403).end();
+                return;
+            }
+        } else {
+            res.status(403).end();
+            return;
+        }
+    } else {
+        res.status(401).end();
+    }
+}
