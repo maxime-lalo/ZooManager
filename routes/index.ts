bbb7867a0ce11ef48bca0b5814a7156583a1c9cd
Maxime LALO <maxime.lalo.pro@gmail.com>
2021-04-25 16:16:52 +0200

    ✨Checking if the zoo is opened if there is enough employees

--- routes/index.ts
+++ routes/index.ts
@@ -3,10 +3,12 @@ import {authRouter} from "./auth.router";
 import {roleRouter} from "./role.router";
 import {spaceRouter} from "./space.router";
 import {passRouter} from "./pass.router";
+import {zooRouter} from "./zoo.router";
 
 export function buildRoutes(app: Express) {
     app.use("/auth", authRouter);
     app.use("/role", roleRouter);
     app.use("/space", spaceRouter);
     app.use("/pass", passRouter);
+    app.use("/zoo", zooRouter)
 }
