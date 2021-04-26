9448c151c23949a0c463fde309d332796f4609e7
Maxime LALO <maxime.lalo.pro@gmail.com>
2021-04-26 21:38:17 +0200

    ✨ Logs for joining space fully working

--- controllers/zoo.controller.ts
+++ controllers/zoo.controller.ts
@@ -1,24 +1,30 @@
 import {ModelCtor} from "sequelize";
 import {SequelizeManager} from "../models";
 import {UserInstance} from "../models/user.model";
+import {SpaceInstance} from "../models/space.model";
+import {SpaceLogsInstance} from "../models/space_logs.model";
 
 export class ZooController {
 
     User: ModelCtor<UserInstance>;
+    Space: ModelCtor<SpaceInstance>;
+    Space_Logs: ModelCtor<SpaceLogsInstance>;
 
     private static instance: ZooController;
 
     public static async getInstance(): Promise<ZooController> {
         if (ZooController.instance === undefined) {
-            const {User} = await SequelizeManager.getInstance();
-            ZooController.instance = new ZooController(User);
+            const {User, Space, Space_Logs} = await SequelizeManager.getInstance();
+            ZooController.instance = new ZooController(User, Space, Space_Logs);
         }
         return ZooController.instance;
 
     }
 
-    private constructor(user: ModelCtor<UserInstance>) {
+    private constructor(user: ModelCtor<UserInstance>, space: ModelCtor<SpaceInstance>, space_logs: ModelCtor<SpaceLogsInstance>) {
         this.User = user;
+        this.Space = space;
+        this.Space_Logs = space_logs;
     }
 
     public async isOpened():Promise<boolean>{
@@ -56,5 +62,74 @@ export class ZooController {
         }
 
         return (hasHostess && hasHealer && hasVendor && hasCleanupAgent);
+    }
+
+    public async joinSpace(userId : number, spaceId : number): Promise<SpaceLogsInstance | null>{
+        const user = await this.User.findOne({
+            where: {
+                "id" : userId
+            }
+        });
+
+        if(user === null){
+            return null;
+        }
+
+        const spaceIn = await this.Space.findOne({
+            where: {
+                "id" : spaceId
+            }
+        });
+
+        if (spaceIn === null){
+            return null;
+        }
+
+        /**
+         * Si le user est dans le zoo, c'est qu'il passe d'un space à un autre
+         * il faut donc dire qu'il sort de son précedent in, log un out et ensuite log un in
+         * Sinon c'est qu'il rentre dans le zoo
+         */
+        if(user.in_zoo){
+            const lastSpaceIn = await this.Space_Logs.findOne({
+                where: {
+                    "in_out": true
+                },
+                include:{
+                    model: this.User,
+                    as: this.User.tableName,
+                    where:{
+                        '$User.id$' : userId
+                    }
+                },
+                order: [
+                    ['timestamp', 'DESC']
+                ]
+            });
+
+            const spaceLogOut = await this.Space_Logs.create({
+                "in_out": false,
+                "timestamp": new Date()
+            });
+
+            spaceLogOut.setSpace(await lastSpaceIn?.getSpace());
+            spaceLogOut.setUser(user);
+            await spaceLogOut.save();
+        }else{
+            user.in_zoo = true;
+            await user.save();
+        }
+
+        // Dans les deux cas on log le in
+        const spaceLogIn = await this.Space_Logs.create({
+            "in_out": true,
+            "timestamp": new Date()
+        });
+
+        spaceLogIn.setSpace(spaceIn);
+        spaceLogIn.setUser(user);
+        await spaceLogIn.save();
+
+        return spaceLogIn;
     }
 }
